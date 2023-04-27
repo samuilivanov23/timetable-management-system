89233d55c1ac34436c40c7f0bd7849e8fa353d5f
samuilivanov23 <samuil2001ivanov@gmail.com>
2023-04-27 19:30:28 +0300

    ***(2023-04-27) ***note=Adding baseic hardcoded timetable visualization ***feature-major

---
+++ project1/src/main/java/com/tms/spring/project/controller/TimetableController.java
@@ -0,0 +1,34 @@
+package com.tms.spring.project.controller;
+
+import com.tms.spring.project.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Controller
+public class HomeController 
+{
+	@GetMapping( "/ShowTimetable" )
+	public String Index( Model model )
+	{
+		//try
+		//{
+			//List<Item> items = itemService.FindAllItems();  
+			//model.addAttribute( "loggedInUser", loggedInUser );
+			//model.addAttribute( "items", items );
+		//}
+		//catch( Exception exception )
+		//{
+		//	exception.printStackTrace();
+		//}
+
+		return "timetable";
+	}
+}
